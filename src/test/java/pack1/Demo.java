5e0e79d69adde8199ce7c387ee41b925eb934988
Amir Pathan <amirpathan52592@gmail.com>
2023-03-05 00:12:07 +0530

    first commit

---
+++ src/test/java/pack1/Demo.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void test() {
+		System.out.println("test 1 executed");
+                System.out.println("added this line");
+                System.out.println("added this once againline");
+	}
+
+}
